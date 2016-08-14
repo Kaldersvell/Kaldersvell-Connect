ab9865df927c25321492c0b8119b91d3577e27d8
ngpaladi <ngpaladi@kaldersvell.org>
2016-08-14 16:26:29 -0400

    Add connection and main menu functionality partially implemented

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const electron = require('electron');
 // Module to control application life.
-const {app} = electron;
+const {
+    app
+} = electron;
 // Module to create native browser window.
-const {BrowserWindow} = electron;
+const {
+    BrowserWindow
+} = electron;
 
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -11,26 +15,27 @@ let win;
 
 
 function createWindow() {
-  //Start AR Drone takeoff sequence
-  //arClient.takeoff();
-  
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`);
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+    // and load the index.html of the app.
+    win.loadURL(`file://${__dirname}/index.html`);
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-  
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+
 }
 
 // This method will be called when Electron has finished
@@ -38,29 +43,29 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-
+app.on('browser-window-created', function (e, window) {
+    window.setMenu(null);
+});
 
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  //arClient.land();
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    //arClient.land();
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-    //arClient.takeoff();
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
 
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
